71e64bbfd9969586a174a1b04a11c0a12ca811a1
Matt Feemster <matt.feemster@gmail.com>
2023-01-16 15:41:32 -0700

    -Further implementation of classes, including __New(), __Init() and super. -Improve numerical parsing to allow for suffixes like L or D. -Fix optimizer to produce a long constant instead of a double when both arguments are long.

---
+++ Keysharp.Tests/ClassTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Keysharp.Tests
+{
+	public partial class Scripting
+	{
+		[Test, Category("Class")]
+		public void Class() => Assert.IsTrue(TestScript("class", false));
+
+		[Test, Category("Class")]
+		public void ClassWithStaticVar() => Assert.IsTrue(TestScript("class-static", false));
+
+		[Test, Category("Class")]
+		public void ClassWithMemberFuncs() => Assert.IsTrue(TestScript("class-member-funcs", false));
+
+		[Test, Category("Class")]
+		public void ClassExtends() => Assert.IsTrue(TestScript("class-extends", false));
+
+		[Test, Category("Class")]
+		public void ClassParams() => Assert.IsTrue(TestScript("class-params", false));
+	}
+}
