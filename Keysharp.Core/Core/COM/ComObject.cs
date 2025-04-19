e1d630a34b8d2d9f69af9b49f927f9888623b26e
Descolada <16986957+Descolada@users.noreply.github.com>
2025-04-19 15:19:23 +0300

    Fixed ComObject destructor

--- Keysharp.Core/Core/COM/ComObject.cs
+++ Keysharp.Core/Core/COM/ComObject.cs
@@ -183,14 +183,13 @@ namespace Keysharp.Core.COM
 
 		~ComObject()
 		{
-			if (Marshal.IsComObject(Ptr))
-				_ = Marshal.ReleaseComObject(Ptr);
-			else if (VarType == Com.vt_unknown || VarType == Com.vt_dispatch)
-			{
-				if (Ptr is long l && l != 0L)
-					_ = Marshal.Release((nint)l);
-				else if (Ptr is IntPtr ip && ip != IntPtr.Zero)
+			if (Ptr == null)
+				return;
+			if (VarType == Com.vt_unknown || VarType == Com.vt_dispatch) {
+				if (Ptr is IntPtr ip)
 					_ = Marshal.Release(ip);
+				else if (Ptr is long lp)
+					_ = Marshal.Release((nint)lp);
 			}
 		}
 
