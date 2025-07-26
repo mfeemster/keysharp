f3c06300179d1acda439c7f7e3f1773a00ef11f2
Descolada <16986957+Descolada@users.noreply.github.com>
2025-07-26 08:16:58 +0300

    Fixed HotVariant throwing because of cast issue

--- Keysharp.Core/Common/Keyboard/HotkeyDefinition.cs
+++ Keysharp.Core/Common/Keyboard/HotkeyDefinition.cs
@@ -1932,7 +1932,7 @@ namespace Keysharp.Core.Common.Keyboard
 				originalCallback = Proc,
 				maxThreads = A_MaxThreadsPerHotkey.Aui(),    // The values of these can vary during load-time.
 				maxThreadsBuffer = A_MaxThreadsBuffer.Ab(),
-				inputLevel = (uint)A_InputLevel,
+				inputLevel = (long)A_InputLevel,
 				hotCriterion = Script.TheScript.Threads.CurrentThread.hotCriterion, // If this hotkey is an alt-tab one (mHookAction), this is stored but ignored until/unless the Hotkey command converts it into a non-alt-tab hotkey.
 				suspendExempt = A_SuspendExempt.Ab(),
 				noSuppress = _noSuppress,
