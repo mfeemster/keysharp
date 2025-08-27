678d9587affd08b6d9054231e8c41055e07e4932
Descolada <16986957+Descolada@users.noreply.github.com>
2025-08-27 14:38:10 +0300

    Remove use of assembly and native memory from DelegateHolder

--- Keysharp.Core/Scripting/Script/Script.cs
+++ Keysharp.Core/Scripting/Script/Script.cs
@@ -86,7 +86,6 @@ namespace Keysharp.Scripting
 		private ComMethodData comMethodData;
 #endif
 		private ControlProvider controlProvider;
-		private DelegateData delegateData;
 #if WINDOWS
 		private DllData dllData;
 #endif
@@ -129,7 +128,6 @@ namespace Keysharp.Scripting
 		internal ComMethodData ComMethodData => comMethodData ?? (comMethodData = new ());
 #endif
 		internal ControlProvider ControlProvider => controlProvider ?? (controlProvider = new ());
-		internal DelegateData DelegateData => delegateData ?? (delegateData = new ());
 #if WINDOWS
 		internal DllData DllData => dllData ?? (dllData = new ());
 #endif
