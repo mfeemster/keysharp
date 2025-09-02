488ca5fb2d892fd0bfb2be8d944d9d6fc06f7fc8
Descolada <16986957+Descolada@users.noreply.github.com>
2025-09-02 17:18:20 +0300

    Add fast failure path for strings in ForceDouble, ForceLong, ForceInt

--- Keysharp.Core/Scripting/Script/Conversion.cs
+++ Keysharp.Core/Scripting/Script/Conversion.cs
@@ -48,6 +48,8 @@ namespace Keysharp.Scripting
 				return l;
 			else if (input.ParseBool() is bool b)
 				return b ? 1.0 : 0.0;
+			else if (input is string)
+				return 0.0;
 			else if (input.GetType().GetMethods(BindingFlags.Static | BindingFlags.Public) is MethodInfo[] mis)
 			{
 				foreach (var mi in mis)
@@ -73,6 +75,8 @@ namespace Keysharp.Scripting
 				return (int)d;
 			else if (input.ParseBool() is bool b)
 				return b ? 1 : 0;
+			else if (input is string)
+				return 0;
 			else if (input.GetType().GetMethods(BindingFlags.Static | BindingFlags.Public) is MethodInfo[] mis)
 			{
 				foreach (var mi in mis)
@@ -98,6 +102,8 @@ namespace Keysharp.Scripting
 				return (long)d;
 			else if (input.ParseBool() is bool b)
 				return b ? 1L : 0L;
+			else if (input is string)
+				return 0;
 			else if (input.GetType().GetMethods(BindingFlags.Static | BindingFlags.Public) is MethodInfo[] mis)
 			{
 				foreach (var mi in mis)
