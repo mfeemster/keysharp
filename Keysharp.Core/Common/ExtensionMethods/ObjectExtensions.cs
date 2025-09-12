87ba266588953a1b9eb89533520688aef0857061
Descolada <16986957+Descolada@users.noreply.github.com>
2025-09-12 21:37:03 +0300

    Add missing requiredot check

--- Keysharp.Core/Common/ExtensionMethods/ObjectExtensions.cs
+++ Keysharp.Core/Common/ExtensionMethods/ObjectExtensions.cs
@@ -226,6 +226,7 @@
 
 			if (obj is long l)
 			{
+				if (requiredot) { outvar = default; return false; }
 				outvar = l;
 				return true;
 			}
@@ -237,6 +238,7 @@
 
 			if (obj is int i)//int is seldom used in Keysharp, so check last.
 			{
+				if (requiredot) { outvar = default; return false; }
 				outvar = i;
 				return true;
 			}
